d06d734bc068de0d22f4c3a761e3876005f9c199
Will Gibbs <will.gibbs7793@gmail.com>
2026-01-11 00:11:40 -0500

    Add comments to test server and client files

--- src/sec_interfaces_test/src/client.cpp
+++ src/sec_interfaces_test/src/client.cpp
@@ -16,24 +16,31 @@ using namespace sec_interfaces::srv;
 // Definition of a test client node
 class TestClient : public Node
 {
+   // Service clients
    Client<RegisterController>::SharedPtr register_controller;
-
    Client<UpdateTask>::SharedPtr         update_task;
 
+   // Wall timer
    TimerBase::SharedPtr                  timer;
 public:
+   // Constructor, create a test client node
    TestClient() : Node("test_client")
    {
+      // Callback for register controller service client
+      // This function will be called when the client receives a response from the server
       auto register_controller_callback = [this](Client<RegisterController>::SharedFuture future_response) -> void
       {
          RCLCPP_INFO(get_logger(), "Received a response with a status code of %d.", future_response.get()->registration_status_code);
       };
 
+      // Callback for update task service client
+      // This function will be called when the client receives a response from the server
       auto update_task_callback = [this](Client<UpdateTask>::SharedFuture future_response) -> void
       {
          RCLCPP_INFO(get_logger(), "Received a response with a status code of %d.", future_response.get()->request_status);
       };
 
+      // Callback function to be called on every tick of the wall timer
       auto timer_callback = [this, register_controller_callback, update_task_callback]() -> void
       {
          // Create service requests
@@ -50,6 +57,8 @@ public:
          update_task_request->new_task_likelihood_of_success = 0.95f;
 
          RCLCPP_INFO(get_logger(), "Sending register controller request...");
+
+         // Wait for the service to become available
          while(!register_controller->wait_for_service(500ms))
          {
             if(ok())
@@ -63,9 +72,12 @@ public:
             }
          }
 
+         // Send the register controller service request
          auto register_controller_response = register_controller->async_send_request(register_controller_request, register_controller_callback);
 
          RCLCPP_INFO(get_logger(), "Sending update task request...");
+
+         // Wait for the service to become available
          while(!update_task->wait_for_service(500ms))
          {
             if(ok())
@@ -79,12 +91,16 @@ public:
             }
          }
 
+         // Send the update task service request
          auto update_task_response = update_task->async_send_request(update_task_request, update_task_callback);
       };
 
+      // Create the clients with the related interface types and service names
+      // create_client(const std::string &service_name)
       register_controller = create_client<RegisterController>("register_controller");
       update_task         = create_client<UpdateTask>("update_task");
 
+      // Create the wall timer
       timer = create_wall_timer(2s, timer_callback);
    }
 };
@@ -93,8 +109,10 @@ int main(int argc, char *argv[])
 {
    init(argc, argv);
 
+   // Execute the node
    spin(make_shared<TestClient>());
 
+   // Clean up
    shutdown();
 
    return 0;
--- src/sec_interfaces_test/src/server.cpp
+++ src/sec_interfaces_test/src/server.cpp
@@ -14,61 +14,61 @@ using namespace sec_interfaces::srv;
 // Definition of a test server node
 class Server : public Node
 {
+   // Service servers
    Service<RegisterController>::SharedPtr register_controller;
-
    Service<UpdateTask>::SharedPtr         update_task;
-
-   // TimerBase::SharedPtr timer;
 public:
+   // Constructor, create a server node
    Server() : Node("test_server")
    {
+      // Callback for register controller service server
       auto register_controller_callback = [this](const shared_ptr<RegisterController::Request> request, shared_ptr<RegisterController::Response> response) -> void
       {
-         string action_name = request->controller_action_name;
-
+         // Modify the response to have a status code of OK
+         // note: this response will be sent to the client when the callback function returns
          response->registration_status_code = response->OK;
 
-         RCLCPP_INFO(get_logger(), "Registration request received with a controller action name of '%s'.", action_name.c_str());
+         // Log the actions of the service
+         RCLCPP_INFO(get_logger(), "Registration request received with a controller action name of '%s'.", (request->controller_action_name).c_str());
          RCLCPP_INFO(get_logger(), "Replying with status code %d (OK)...", response->OK);
       };
 
+      // Callback for update task service server
       auto update_task_callback = [this](const shared_ptr<UpdateTask::Request> request, shared_ptr<UpdateTask::Response> response) -> void
       {
-         string action_name = request->controller_action_name;
-
+         // Modify the response to have a status code of OK
+         // note: this response will be sent to the client when the callback function returns
          response->request_status = response->OK;
 
+         // Log the actions of the service
          RCLCPP_INFO(get_logger(), "Update task request received with values: controller_action_name='%s', new_task_point_value=%f, new_task_time_to_complete=%f, new_task_likelihood_of_success=%f.",
-                                    action_name.c_str(),
+                                    (request->controller_action_name).c_str(),
                                     request->new_task_point_value,
                                     request->new_task_time_to_complete,
                                     request->new_task_likelihood_of_success);
          RCLCPP_INFO(get_logger(), "Replying with status code %d (OK)...", response->OK);
       };
 
+      // Create the services with the related interface types, service names, and callback functions
+      // create_service(const std::string &service_name, CallbackT &&callback)
       register_controller = create_service<RegisterController>("register_controller", register_controller_callback);
       update_task = create_service<UpdateTask>("update_task", update_task_callback);
 
+      // Log the actions of the node
       RCLCPP_INFO(get_logger(), "Creating a test server...");
       RCLCPP_INFO(get_logger(), "Done.");
       RCLCPP_INFO(get_logger(), "Waiting for a request...");
    }
-
-   /*void register_controller_callback(const shared_ptr<RegisterController::Request> request, shared_ptr<RegisterController::Response> response)
-   {
-      response->registration_status_code = response->OK;
-
-      RCLCPP_INFO(get_logger(), "Registration request recieved...");
-      RCLCPP_INFO(get_logger(), "Replying with status code %d (OK)...", response->OK);
-   }*/
 };
 
 int main(int argc, char *argv[])
 {
    init(argc, argv);
 
+   // Execute the node
    spin(make_shared<Server>());
 
+   // Clean up
    shutdown();
 
    return 0;
