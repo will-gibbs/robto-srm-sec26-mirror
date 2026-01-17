1d903d61dbff097b142530c713251a5ca7760f5a
Joseph McCurdy <joseph.mccurdy@students.pcci.edu>
2026-01-17 13:18:25 -0600

    Adds a timekeeper package that keeps track of the round time for  for system.

---
+++ src/timekeeper/src/tk_server.cpp
@@ -0,0 +1,108 @@
+// C++-specific packages
+#include <chrono>
+#include <memory>
+
+// ROS2-specific packages
+#include "rclcpp/rclcpp.hpp"
+#include "builtin_interfaces/msg/duration.hpp"
+#include "sec_interfaces/srv/start_round.hpp"
+
+// Namespaces
+using namespace std;
+using namespace std::chrono_literals;
+using namespace rclcpp;
+
+// The definition of a timekeeper node
+class Timekeeper : public Node
+{
+   // Wall timer
+   TimerBase::SharedPtr                                    timer;
+
+   // Round start client
+   Client<sec_interfaces::srv::StartRound>::SharedPtr     start_round;
+   
+   // Round time publisher
+   Publisher<builtin_interfaces::msg::Duration>::SharedPtr publisher;
+
+   // Time remaining in the round in seconds and nanoseconds
+   int32_t                                                 round_time_sec;
+   uint32_t                                                round_time_nanosec;
+public:
+   // Constructor, create a timekeeper
+   Timekeeper() : Node("timekeeper"), round_time_sec(180), round_time_nanosec(0)
+   {
+      // Create the round start client
+      start_round = create_client<sec_interfaces::srv::StartRound>("start_round");
+
+      // Wait for start_round to be available
+      while (!start_round->wait_for_service(1s))
+      {
+         if (!rclcpp::ok())
+         {
+            RCLCPP_ERROR(get_logger(), "Interrupted while waiting for start_round.");
+            return;
+         }
+         RCLCPP_INFO(get_logger(), "Waiting for start_round...");
+      }
+
+      // Create the request to start the round
+      auto request = std::make_shared<sec_interfaces::srv::StartRound::Request>();
+
+      // Populate request fields here if required
+       request->start_round = 1;
+
+      // Call the service
+      auto future = start_round->async_send_request(request);
+
+      // Optional: confirm the call was sent
+      RCLCPP_INFO(get_logger(), "start_round called.");
+
+      // Create the round time publisher
+      publisher = create_publisher<builtin_interfaces::msg::Duration>("round_time", 10);
+
+      // Callback function to be called on every tick of the wall timer
+      auto timer_callback = [this]() -> void
+      {
+         // Create the message to be published
+         auto message = builtin_interfaces::msg::Duration();
+
+         // Write the time remaining to the message
+         message.sec = round_time_sec;
+         message.nanosec = round_time_nanosec;
+
+         // Log the time remaining
+         RCLCPP_INFO(get_logger(), "Time remaining: %d.%02.0f seconds.",
+            message.sec, (float)message.nanosec * 0.0000001f);
+         
+         // Publish the message
+         publisher->publish(message);
+
+         // Subtract time elapsed from the time remaining
+         if (round_time_nanosec == (uint32_t)0)
+         {
+            round_time_sec -= 1;
+            round_time_nanosec = 990000000;
+         }
+         else
+         {
+            round_time_nanosec -= 10000000;
+         }
+      };
+
+      // Create the wall timer
+      timer = create_wall_timer(10ms, timer_callback);
+   }
+};
+
+int main(int argc, char *argv[])
+{
+   init(argc, argv);
+
+   // Execute the node
+   spin(make_shared<Timekeeper>());
+
+   // Clean up
+   shutdown();
+
+   return 0;
+}
