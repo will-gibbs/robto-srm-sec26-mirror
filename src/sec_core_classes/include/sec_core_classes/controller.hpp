c6a66fb6e53282e7cbe0eb4f3cdffd194dfb7071
MightyT-2 <timmightyt2@gmail.com>
2026-01-16 23:48:30 -0500

    Begin Defining Controller Class

--- src/sec_core_classes/include/sec_core_classes/controller.hpp
+++ src/sec_core_classes/include/sec_core_classes/controller.hpp
@@ -2,6 +2,7 @@
 #include "rclcpp/rclcpp.hpp"
 #include "sec_interfaces/srv/register_controller.hpp"
 #include "sec_interfaces/srv/update_task.hpp"
+#include <string>
 
 // Namespaces
 using namespace std;
@@ -11,5 +12,24 @@ using namespace sec_interfaces::srv;
 
 class Controller : public Node
 {
-    
+    rclcpp_action::Server complete_task;
+    Client                register_controller;
+    Client                update_task;
+    string                action_name;
+    TimerBase             timer;
+
+    void timer_callback() {};
+
+public:
+
+    Controller(string name) : Node(name)
+    {
+        action_name = name;
+    };
+
+    handle_goal() {};
+
+    handle_cancel() {};
+
+    handle_accepted() {};
 };
