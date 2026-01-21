affc93d40e0e790c6a115438c46ac2012b43e8fa
Will Gibbs <will.gibbs7793@gmail.com>
2026-01-21 15:35:34 -0600

    Add logging and priority calculation to controller reference test code; add documentation to controller reference test code

--- src/controller_reference_test/src/controller_reference_test.cpp
+++ src/controller_reference_test/src/controller_reference_test.cpp
@@ -6,6 +6,7 @@
 
 // ROS2-specific packages
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
 
 // Custom packages
 #include "sec_core_classes/controller_reference.hpp"
@@ -18,60 +19,76 @@ using namespace std::chrono_literals;
 // The definition of a controller reference test class
 class ControllerReferenceTest : public Node
 {
+   // Test ControllerReference objects
    ControllerReference controller_1,
                        controller_2;
 
-   TimerBase::SharedPtr timer;
 public:
+   // Interface type aliases
    using UpdateTask   = sec_interfaces::srv::UpdateTask;
    using CompleteTask = sec_interfaces::action::CompleteTask;
 
    // Constructor, create a controller reference test node
-   /* ControllerReferenceIDK() : Node("controller_reference_test")
-   {
-      RCLCPP_INFO(get_logger(), "Creating a controller reference test node...");
-
-      controller_1("controller_1", shared_from_this());
-      controller_2("controller_2", shared_from_this());
-   }*/
    ControllerReferenceTest() : Node("controller_reference_test"),
       controller_1("controller_1"), controller_2("controller_2")
    {
       RCLCPP_INFO(get_logger(), "Creating a controller reference test node...");
 
+      // Initialize the test ControllerReference objects
       init_controller_reference(&controller_1);
       init_controller_reference(&controller_2);
    };
 
+   // Create the service server and action client on a controller reference object
    void init_controller_reference(ControllerReference *controller)
    {
-      auto service_callback = [controller](const shared_ptr<UpdateTask::Request> request, shared_ptr<UpdateTask::Response> response)
+      // Callback for the update task service on the controller reference object
+      auto service_callback = [this, controller](const shared_ptr<UpdateTask::Request> request, shared_ptr<UpdateTask::Response> response)
       {
+         // Log the update task request
+         RCLCPP_INFO(get_logger(), "Received request to update task values from '%s'.", controller->get_controller_name().c_str());
+
+         // Call the callback function that is a member of the ControllerReference object
+         // to update the controller's task values
          controller->controller_update_task_callback(request, response);
+
+         RCLCPP_INFO(get_logger(), "Request handled. request_status = %d", response->request_status);
+
+         if (response->request_status == response->OK)
+         {
+            // Calculate and log the new task priority
+            float calculated_priority = (request->new_task_point_value / request->new_task_time_to_complete) * request->new_task_likelihood_of_success;
+            RCLCPP_INFO(get_logger(), "New calculated priority = %f", calculated_priority);
+         }
+         else if (response->request_status == response->RETRY)
+         {
+            RCLCPP_ERROR(get_logger(), "Error updating task values. Retry update.");
+         }
       };
 
-      RCLCPP_INFO(get_logger(), "Initializing controller reference (%s)...", controller->get_controller_action_name().c_str());
-      controller->set_controller_update_task(create_service<UpdateTask>(controller->get_controller_action_name().c_str() + (string)"_update_task", service_callback));
+      RCLCPP_INFO(get_logger(), "Initializing controller reference (%s)...", controller->get_controller_name().c_str());
+
+      // Crete the service server and action client
+      //
+      // Note: a ControllerReference object cannot do these because it is not a Node object.
+      // Thus, the service server and action client are members of the ControllerReference object,
+      // but are created and handled by a Node object
+      // (in this case, a ControllerReferenceTest object; in actual implementation, the Manager).
+      controller->set_controller_update_task  (create_service<UpdateTask>(controller->get_controller_name().c_str() + (string)"/update_task", service_callback));
+      controller->set_controller_complete_task(rclcpp_action::create_client<CompleteTask>(this, controller->get_controller_name() + "/complete_task"));
+
       RCLCPP_INFO(get_logger(), "Done.");
    };
-
-   /*void set_controller_manager_node()
-   {
-      controller_1.set_manager_node(shared_from_this());
-      controller_1.set_manager_node(shared_from_this());
-   };*/
 };
 
 int main(int argc, char *argv[])
 {
    init(argc, argv);
 
-   /*shared_ptr<ControllerReferenceTest> node = make_shared<ControllerReferenceTest>();
-   node->set_controller_manager_node();
-   spin(node);*/
-
+   // Execute the node
    spin(make_shared<ControllerReferenceTest>());
 
+   // Clean up
    shutdown();
 
    return 0;
