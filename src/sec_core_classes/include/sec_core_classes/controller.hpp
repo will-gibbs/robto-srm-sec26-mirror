e3c8412fa5aaf209b4e0c37c01cba1b37503e265
Will Gibbs <will.gibbs7793@gmail.com>
2026-01-20 20:08:06 -0600

    Refactor `Controller` class

--- src/sec_core_classes/include/sec_core_classes/controller.hpp
+++ src/sec_core_classes/include/sec_core_classes/controller.hpp
@@ -1,44 +1,79 @@
 // This is bad code; this is not complete
 
+// C++-specific packages
+#include <functional>
+#include <memory>
+#include <chrono>
+// #include <string>
+
 // ROS2-specific packages
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
 #include "sec_interfaces/srv/register_controller.hpp"
 #include "sec_interfaces/srv/update_task.hpp"
-#include <string>
+#include "sec_interfaces/action/complete_task.hpp"
 
 // Namespaces
 using namespace std;
 using namespace std::chrono_literals;
+using namespace std::placeholders;
 using namespace rclcpp;
 using namespace sec_interfaces::srv;
 
 class Controller : public Node
 {
-    Server    complete_task;
-    Client    register_controller;
-    Client    update_task;
-    string    action_name;
-    TimerBase timer;
-
-    virtual void timer_callback();
-
 public:
+    using CompleteTask = sec_interfaces::action::CompleteTask;
+    using GoalHandleCompleteTask = rclcpp_action::ServerGoalHandle<CompleteTask>;
 
-    Controller(string name) : Node(name)
+    virtual rclcpp_action::GoalResponse handle_goal(const rclcpp_action::GoalUUID & uuid, shared_ptr<const CompleteTask::Goal> goal)
     {
-        action_name = name;
-        
-        this->action_server_ = rclcpp_action::create_server<Control>(
-            this,
-            action_name,
-            this->handle_goal,
-            this->handle_cancel,
-            this->handle_accepted);
+        RCLCPP_INFO(get_logger(), "Received goal with begin task value of %d", goal->begin_task);
+        (void)uuid;
+        return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
     };
 
-    virtual void handle_goal();
+    virtual rclcpp_action::CancelResponse handle_cancel(const shared_ptr<GoalHandleCompleteTask> goal_handle)
+    {
+        RCLCPP_INFO(get_logger(), "Received cancel request.");
+        (void)goal_handle;
+        return rclcpp_action::CancelResponse::ACCEPT;
+    };
 
-    virtual void handle_cancel();
+    virtual void handle_accepted(const shared_ptr<GoalHandleCompleteTask> goal_handle)
+    {
+        RCLCPP_INFO(get_logger(), "Accepted a goal.");
+        auto execute_in_thread = [this, goal_handle](){return this->execute(goal_handle);};
+        thread{execute_in_thread}.detach();
+    };
 
-    virtual void handle_accepted();
+    virtual void execute(const shared_ptr<GoalHandleCompleteTask> goal_handle)
+    {
+        RCLCPP_INFO(this->get_logger(), "Executing goal");
+        const auto goal = goal_handle->get_goal();
+    }
+
+    Controller(const string name, std::chrono::milliseconds tick_rate) : Node(name)
+    {
+        action_name = name;
+        timer_tick_rate = tick_rate;
+
+        this->complete_task = rclcpp_action::create_server<CompleteTask>(
+            this,
+            name,
+            [this](const auto & uuid, const auto & goal) {return handle_goal(uuid, goal);},
+            [this](const auto & goal_handle) {return handle_cancel(goal_handle);},
+            [this](const auto & goal_handle) {handle_accepted(goal_handle);});
+        
+        timer = create_wall_timer(timer_tick_rate, [this]() {return timer_callback();});
+    };
+private:
+    rclcpp_action::Server<CompleteTask>::SharedPtr    complete_task;
+    Client<RegisterController>::SharedPtr             register_controller;
+    Client<UpdateTask>::SharedPtr                     update_task;
+    string                                            action_name;
+    TimerBase::SharedPtr                              timer;
+    std::chrono::milliseconds                         timer_tick_rate;
+
+    virtual void timer_callback() {};
 };
