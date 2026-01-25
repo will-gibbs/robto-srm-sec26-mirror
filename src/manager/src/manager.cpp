0cba83da766f077819ac60d7a5a9184212a9e340
Will Gibbs <will.gibbs7793@gmail.com>
2026-01-24 23:03:47 -0600

    Make manager functional

--- src/manager/src/manager.cpp
+++ src/manager/src/manager.cpp
@@ -16,8 +16,13 @@
 //******************************************************************************
 
 #include <iostream>
+#include <memory>
+#include <chrono>
 #include <vector>
+
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
+#include "builtin_interfaces/msg/duration.hpp"
 #include "sec_interfaces/srv/register_controller.hpp"
 #include "sec_interfaces/srv/start_round.hpp"
 #include "sec_core_classes/controller_reference.hpp"
@@ -26,6 +31,10 @@
 // RegisterController
 using namespace std;
 using namespace rclcpp;
+using namespace std::chrono_literals;
+
+// Symbolic constants
+#define TICK_RATE 100ms
 
 // Aliases
 using RegisterController = sec_interfaces::srv::RegisterController;
@@ -41,26 +50,30 @@ class Manager : public Node
    // ? For now, p_controllers will be an array of integers
    // ? All instances should be changed to ControllerReference object pointers once that class can be implemented
    vector<ControllerReference>         controllers;           // List of Robto's controllers
-   Subscription<builtin_interfaces::msg::Duration>::SharedPointer
-                       round_time_subscriber; // Subscription to the time remaining in the round
+   Subscription<builtin_interfaces::msg::Duration>::SharedPtr round_time_subscriber; // Subscription to the time remaining in the round
 
-   Service<RegisterController>::SharedPointer register_controller;
-   Service<StartRound>::SharedPointer         start_round;
+   Service<RegisterController>::SharedPtr register_controller;
+   Service<StartRound>::SharedPtr         start_round;
+
+   // Timer
+   TimerBase::SharedPtr                       timer;
 
    // ?
    void register_controller_callback(
-      const RegisterController::Request  request,
-            RegisterController::Response response
+      const shared_ptr<RegisterController::Request>  request,
+            shared_ptr<RegisterController::Response> response
    );
    // ? start_round_callback();
    void start_round_callback(
-      const StartRound::Request  request,
-            StartRound::Response response
+      const shared_ptr<StartRound::Request>  request,
+            shared_ptr<StartRound::Response> response
    );
    // Add a controller to the list
-   void add_controller();
+   void add_controller(ControllerReference new_controller);
    // Sort the controllers into descending order of priority
    void sort_controllers();
+
+   void timer_callback();
 public:
    // Constructor
    Manager();
@@ -72,27 +85,27 @@ public:
 Manager::Manager() : Node("manager")
 {
    // Create services for registering controllers and starting the round
-   register_controller = 
-      node->create_service<RegisterController>
+   register_controller = create_service<RegisterController>
       ("register_controller",
-       [this](const RegisterController::Request  request,
-                    RegisterController::Response response) {Manager::register_controller_callback(request, response);});
-   start_round =
-      node->create_service<StartRound>
+       [this](const shared_ptr<RegisterController::Request>  request,
+                    shared_ptr<RegisterController::Response> response) {Manager::register_controller_callback(request, response);});
+   start_round = create_service<StartRound>
          ("start_round",
-         [this](const StartRound::Request  request,
-                      StartRound::Response response) {Manager::start_round_callback(request, response);});
+         [this](const shared_ptr<StartRound::Request>  request,
+                      shared_ptr<StartRound::Response> response) {Manager::start_round_callback(request, response);});
 
    // Create round time subscriber
-   round_time_subscriber = create_subscription<builtin_interfaces::msg::Duration>("round_time", 10, void);
+   round_time_subscriber = create_subscription<builtin_interfaces::msg::Duration>("round_time", 10, [this](builtin_interfaces::msg::Duration::UniquePtr message) {RCLCPP_INFO(get_logger(), "Message: %d, %d", message->sec, message->nanosec);});
+
+   timer = create_wall_timer(TICK_RATE, [this]() {timer_callback();});
 }
 
 //******************************************************************************
 //* ? *
 //******************************************************************************
 void Manager::register_controller_callback(
-   const RegisterController::Request  request,
-         RegisterController::Response response)
+   const shared_ptr<RegisterController::Request>  request,
+         shared_ptr<RegisterController::Response> response)
 {
    // float priority; // Priority of the controller's task
 
@@ -100,34 +113,34 @@ void Manager::register_controller_callback(
    // priority = (int)request->controller_action_name[0];
 
    // Create a new controller reference
-   ControllerReference new_controller_reference = new ControllerReference(request->controller_action_name);
+   ControllerReference new_controller_reference(request->controller_action_name);
 
    // Add the new controller to the list
    add_controller(new_controller_reference); //? will be replaced with the ControllerReference object
    // ? TODO: Add error handling
-   response->regisration_status_code == response->OK;
-   RCLCPP_INFO(get_logger("rclcpp"), "Added the controller %s", request->controller_action_name);
+   response->registration_status_code = response->OK;
+   RCLCPP_INFO(get_logger(), "Added the controller %s", request->controller_action_name.c_str());
    return;
 }
 
 //******************************************************************************
 //*                           Start Robto's mission                            *
 //******************************************************************************
-void start_round_callback(
-   const StartRound::Request  request,
-         StartRound::Response response)
+void Manager::start_round_callback(
+   const shared_ptr<StartRound::Request>  request,
+         shared_ptr<StartRound::Response> response)
 {
    if (request->start_round == request->START_ROUND)
    {
       round_has_started        = 1;
       response->manager_status = response->OK;
-      RCLCPP_INFO(get_logger("rclcpp"), "Starting Robto's mission. Hang in there, Astroducks!");
+      RCLCPP_INFO(get_logger(), "Starting Robto's mission. Hang in there, Astroducks!");
       // ? Start round logic
    }
    else
    {
       response->manager_status = response->ERROR;
-      RCLCPP_INFO(get_logger("rclcpp"), "Something went wrong with starting the round.");
+      RCLCPP_INFO(get_logger(), "Something went wrong with starting the round.");
    }
    return;
 }
@@ -150,8 +163,24 @@ void Manager::sort_controllers()
    sort(controllers.begin(), controllers.end(), 
       [](auto& first_controller, auto& last_controller)
       {
-         return first_controller->tast_priority > last_controller->task_priority;
+         return first_controller.get_task_priority() > last_controller.get_task_priority();
       });
+   return;
+}
+
+
+//******************************************************************************
+//*               Callback function for every tick of the timer                *
+//******************************************************************************
+void Manager::timer_callback()
+{
+   RCLCPP_INFO(get_logger(), "AAAAAAAAHHHHHHHH");
+
+   /*if (round_has_started)
+   {
+
+   }*/
+
    return;
 }
 
