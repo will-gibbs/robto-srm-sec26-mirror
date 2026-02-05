a9eee966e4c89c81d361b054ec39598cbaa3d278
Will Gibbs <will.gibbs7793@gmail.com>
2026-02-05 16:21:06 -0600

    Add `HardwareDriver` class to `sec_core_classes`

---
+++ src/sec_core_classes/include/sec_core_classes/hardware_driver.hpp
@@ -0,0 +1,56 @@
+#ifndef HARDWARE_DRIVER_HPP
+#define HARDWARE_DRIVER_HPP
+
+// Symbolic constants
+#define AS_IS  1
+#define INPUT  2
+#define OUTPUT 3
+
+// C++-specific packages
+#include <memory>
+#include <gpiod.hpp>
+
+// ROS2-specific packages
+#include "rclcpp/rclcpp.hpp"
+
+// Namespaces --- NOS
+using namespace std;
+using namespace rclcpp;
+
+// Defintion of a general hardware driver node
+class HardwareDriver : public Node
+{
+   
+public:
+   // Constructor, create a hardware driver
+   HardwareDriver(const string name) : Node(name)
+   {
+      RCLCPP_INFO(get_logger(), "Creating a hardware driver: '%s'.", name.c_str());
+      init_gpios();
+   }
+
+   // Destructor, delete a hardware driver
+   ~HardwareDriver()
+   {
+      RCLCPP_INFO(get_logger(), "Deleting a hardware driver");
+   }
+
+   // Set the mode of a specific gpio pin
+   int set_pin_mode(int pin_number, int direction)
+   {
+      RCLCPP_INFO(get_logger(), "Setting pin mode: pin_number=%d, direction=%d", pin_number, direction);
+
+      return 0;
+   }
+
+   // Set the modes of all gpio pins the driver uses
+   virtual int init_gpios()
+   {
+      RCLCPP_INFO(get_logger(), "Initializing gpio pins...");
+      set_pin_mode(17, (int)gpiod::line_request::DIRECTION_OUTPUT);
+
+      return 0;
+   }
+};
+
+#endif
