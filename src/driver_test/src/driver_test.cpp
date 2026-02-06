15f6f3df8189b088908e02c12b3982f84d305065
Will Gibbs <will.gibbs7793@gmail.com>
2026-02-06 16:49:53 -0600

    Add comments to `DriverTest` class

--- src/driver_test/src/driver_test.cpp
+++ src/driver_test/src/driver_test.cpp
@@ -1,13 +1,18 @@
+// C++-specific packages
 #include <memory>
 
+// ROS2-specific packages
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/bool.hpp"
 
+// Custom packages
 #include "sec_core_classes/hardware_driver.hpp"
 
+// Namespaces
 using namespace std;
 using namespace rclcpp;
 
+// Defintion of a test hardware driver node
 class DriverTest : public HardwareDriver
 {
    // GPIO line for the LED
@@ -16,6 +21,7 @@ class DriverTest : public HardwareDriver
    // Subscriber to blink the LED
    Subscription<std_msgs::msg::Bool>::SharedPtr blink_subscriber;
 public:
+   // Constructor, create a test hardware driver
    DriverTest() : HardwareDriver("driver_test")
    {
       // Callback for the blink subscriber
@@ -35,6 +41,7 @@ public:
          }
       };
 
+      // Initialize the GPIO pins
       if (init_gpios() == 0)
       {
       	RCLCPP_INFO(get_logger(), "GPIO pins initialization successful.");
@@ -42,7 +49,7 @@ public:
       else
       {
          RCLCPP_ERROR(get_logger(), "Error: failed to initialize GPIO pins. Exiting");
-	 exit(1);
+	      exit(1);
       }
 
       // Initialize the blink subscriber
@@ -50,20 +57,25 @@ public:
          ("blink", 10, blink_callback);
    }
 
+   // Set the modes of all GPIO pins the driver uses
    int init_gpios() override
    {
-      line = set_pin_mode(4, (int)gpiod::line_request::DIRECTION_OUTPUT);
+      line = set_pin_mode(4, OUTPUT);
 
       return 0;
    }
 };
 
+// Main function
 int main(int argc, char *argv[])
 {
+   // Initialize ROS2
    init(argc, argv);
 
+   // Execute the node
    spin(make_shared<DriverTest>());
 
+   // Clean up
    shutdown();
 
    return 0;
