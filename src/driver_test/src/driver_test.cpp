2ac26e0bf545eb200e196ac62eb650217e23a436
Will Gibbs <will.gibbs7793@gmail.com>
2026-02-06 16:08:19 -0500

    Add blink subscriber to driver test

--- src/driver_test/src/driver_test.cpp
+++ src/driver_test/src/driver_test.cpp
@@ -1,6 +1,7 @@
 #include <memory>
 
 #include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/bool.hpp"
 
 #include "sec_core_classes/hardware_driver.hpp"
 
@@ -9,10 +10,31 @@ using namespace rclcpp;
 
 class DriverTest : public HardwareDriver
 {
+   // GPIO line for the LED
    gpiod::line line;
+
+   // Subscriber to blink the LED
+   Subscription<std_msgs::msg::Bool>::SharedPtr blink_subscriber;
 public:
    DriverTest() : HardwareDriver("driver_test")
    {
+      // Callback for the blink subscriber
+      auto blink_callback = [this](std_msgs::msg::Bool::UniquePtr message) -> void
+      {
+         if (message->data == 1)
+         {
+            // Turn the LED on
+            RCLCPP_INFO(get_logger(), "LED on...");
+	    line.set_value(1);
+         }
+         else
+         {
+            // Turn the LED off
+            RCLCPP_INFO(get_logger(), "LED off...");
+            line.set_value(0);
+         }
+      };
+
       if (init_gpios() == 0)
       {
       	RCLCPP_INFO(get_logger(), "GPIO pins initialization successful.");
@@ -22,6 +44,10 @@ public:
          RCLCPP_ERROR(get_logger(), "Error: failed to initialize GPIO pins. Exiting");
 	 exit(1);
       }
+
+      // Initialize the blink subscriber
+      blink_subscriber = create_subscription<std_msgs::msg::Bool>
+         ("blink", 10, blink_callback);
    }
 
    int init_gpios() override
