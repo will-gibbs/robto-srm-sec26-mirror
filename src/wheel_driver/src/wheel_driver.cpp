e0b8f4413fe42c84f90f1cf92739206e07209ace
Ethan Black <golemwire@gmail.com>
2026-02-21 22:21:04 -0600

    Initial draft / template filling-in.
    Fails to compile, due to not being able to find "geometry_msgs/msg/twist.hpp" (line 6).

--- src/wheel_driver/src/wheel_driver.cpp
+++ src/wheel_driver/src/wheel_driver.cpp
@@ -1,21 +1,19 @@
-// C++-specific packages
 #include <memory>
 
 // ROS2-specific packages
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+//#include "std_msgs/msg/string.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 
 using namespace std;
 using namespace rclcpp;
 
-// The definition of a node that subscribes to a topic of odd and even integers
-class OddEvenSubscriber : public Node
+class WheelDriver : public Node
 {
-   Subscription<std_msgs::msg::String>::SharedPtr subscription;
+   Subscription<geometry_msgs::msg::Twist>::SharedPtr subscr;
 public:
-   // Constructor, create an odd-even subscriber node; call the Node constructor
    // Node(const std::string &node_name)
-   OddEvenSubscriber() : Node("ethan_black_subscriber")
+   WheelDriver() : Node("wheel_driver")
    {
       // Callback function to be called every time the subscriber receives a message
       auto topic_callback = [this](std_msgs::msg::String::UniquePtr message) -> void
@@ -24,19 +22,24 @@ public:
          // RCLCPP_INFO(rclcpp::Logger logger, std::string log_message)
          RCLCPP_INFO(get_logger(), "I have heard the next number in the sequence, and that number is %s.", message->data.c_str());
       };
-
+      
       // Create a subscriber on the node
+      const rclcpp::QoS qos = topic_qos();
       // create_subscriber(const std::string &topic_name, rclcpp::QoS QoS, CallbackT && callback)
-      subscription = create_subscription<std_msgs::msg::String>("ethan_black_topic", 10, topic_callback);
+      subscr = create_subscription<geometry_msgs::msg::Twist>(/*`real_name "/" + `?*/"cmd_vel", /*`10`?*/qos, topic_callback);
+      /*velocity_sub_ = nh_->create_subscription<geometry_msgs::msg::Twist>(
+       real_name + "/cmd_vel", qos, std::bind(
+         &Turtle::velocityCallback, this,
+         std::placeholders::_1))*/
    }
 };
 
 int main(int argc, char * argv[])
 {
    init(argc, argv);
-
+   
    // Start up the node
-   spin(make_shared<OddEvenSubscriber>());
+   spin(make_shared<WheelDriver>());
 
    // Clean up
    shutdown();
