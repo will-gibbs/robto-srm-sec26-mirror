bffe2884c788d8eb6bae7fe28b357301d4748005
Will Gibbs <will.gibbs7793@gmail.com>
2026-03-07 15:51:57 -0600

    Finish refactoring and add PWM control

--- src/wheel_driver/setup.py
+++ src/wheel_driver/setup.py
@@ -24,6 +24,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+           'wheel_driver = wheel_driver.wheel_driver:main'
         ],
     },
 )
--- src/wheel_driver/wheel_driver/wheel_driver.py
+++ src/wheel_driver/wheel_driver/wheel_driver.py
@@ -1,37 +1,83 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int8
+from geometry_msgs.msg import Twist
+
+from gpiozero import LED
+from gpiozero import PWMOutputDevice
+from time import sleep
+
+forward = LED(27)
+backward = LED(17)
+pwm = PWMOutputDevice(14, frequency=50)
+
+forward.on()
+backward.off()
+
+pwm.value = 1
+
+sleep(5)
+
+pwm.value = 0
+sleep(1)
 
 # from rpi_hardware_pwm import HardwarePWM
 
-class PWMTest(Node):
+class DrivetrainAction():
    def __init__(self):
-      super().__init__("pwm_test")
-      self.counter = 0
-      self.publisher = self.create_publisher(Int8, "test_topic", 10)
-      self.subscriber = self.create_subscription(
-         Int8,"pwm_control", self.pwm_listener_callback, 10)
-      self.timer = self.create_timer(1, self.timer_callback)
+      self.left_motor = 0.0
+      self.right_motor = 0.0
 
-   def pwm_listener_callback(self, pwm_msg):
-      self.get_logger().info(f"Received: {pwm_msg.data}.")
+   def clamp_motor_value(self, motor, min_value, max_value):
+      if (motor == "left"):
+         return max(min_value, min(self.left_motor, max_value))
+      elif (motor == "right"):
+         return max(min_value, min(self.right_motor, max_value))
 
-   def timer_callback(self):
-      msg = Int8()
-      msg.data = self.counter
-      self.publisher.publish(msg)
-      self.get_logger().info(f"Publishing: {self.counter}...")
-      self.counter += 1
+class WheelDriver(Node):
+   def __init__(self):
+      super().__init__("wheel_driver")
+      self.vel_subscriber = self.create_subscription(
+         Twist,"cmd_vel", self.twist_callback, 10)
 
+   def twist_callback(self, vel):
+      dta = DrivetrainAction
+      self.get_logger().info(f"Linear: {vel.linear} | Angular: {vel.angular}\r\n")
+      dta = self.calc_drivetrain_action(vel.linear.x, vel.angular.z)
+      self.get_logger().info(f"Left motor: {dta.left_motor} | Right motor: {dta.right_motor}")
+
+      if (dta.left_motor > 0):
+         forward.on()
+         backward.off()
+      elif (dta.left_motor < 0):
+         forward.off()
+         backward.on()
+      else:
+         forward.off()
+         backward.off()
+      
+      pwm.value = dta.left_motor
+
+   def calc_drivetrain_action(self, linear, angular) -> DrivetrainAction:
+      dta = DrivetrainAction()
+
+      dta.left_motor = +linear - angular
+      dta.right_motor = +linear + angular
+
+      excession = (dta.clamp_motor_value("left", -1.0, 1.0) - dta.left_motor) + \
+                  (dta.clamp_motor_value("right", -1.0, 1.0) - dta.right_motor)
+      dta.left_motor += excession
+      dta.right_motor += excession
+
+      return dta
 
 def main(args=None):
    rclpy.init(args=args)
    
-   pwm_test_node = PWMTest()
+   wheel_driver = WheelDriver()
    
-   rclpy.spin(pwm_test_node)
+   rclpy.spin(wheel_driver)
 
-   pwm_test_node.destroy_node()
+   wheel_driver.destroy_node()
    rclpy.shutdown()
 
 
