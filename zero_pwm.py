248f19ab4be7a14ac34cfe28e8aee0811095b690
Will Gibbs <will.gibbs7793@gmail.com>
2026-03-06 22:24:31 -0600

    Add software pwm to zero_pwm.py test file

--- zero_pwm.py
+++ zero_pwm.py
@@ -1,12 +1,15 @@
 from gpiozero import Servo
 from gpiozero import Motor
 from gpiozero import LED
+from gpiozero import PWMOutputDevice
 from time import sleep
 
-forward = LED(0)
-backward = LED(1)
-servo = Servo(15)
-left_motor = Motor(17, 27)
+forward = LED(27)
+backward = LED(17)
+#servo = Servo(20)
+#left_motor = Motor(17, 27)
+#left_wheel_pwm = Servo(14)
+pwm = PWMOutputDevice(14, frequency=50)
 
 #while True:
 #    servo.min()
@@ -18,10 +21,27 @@ left_motor = Motor(17, 27)
 
 #forward.on()
 #backward.off()
-left_motor.forward()
-while True:
+#left_motor.forward()
+forward.on()
+backward.off()
+#pwm.value = 1
+#off = 0.0
+#on = 1.0
+#step = 0.1
+pwm.value = 1
+#while (pwm.value < on):
+#    pwm.value += step
+#    sleep(0.1)
+sleep(5)
+#while True:
+#    left_wheel_pwm.value = 1
+#    left_motor.forward()
 #    servo.value = 0.1
-    left_motor.value = 1
-#servo.mid()
-#sleep(0.5)
+#    left_motor.value = 1
+#    servo.mid()
+#    sleep(1)
+#    servo.max()
+#    sleep(1)
 #servo.value = 0
+pwm.value = 0
+sleep(1)
