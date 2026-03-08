7d82e8dc66d00c2709a1df5c631000237cc934f7
Will Gibbs <will.gibbs7793@gmail.com>
2026-03-07 20:03:34 -0600

    Correct syntax errors

--- src/start_switch_driver/start_switch_driver/start_switch_driver.py
+++ src/start_switch_driver/start_switch_driver/start_switch_driver.py
@@ -5,11 +5,9 @@ import rclpy
 from rclpy.node import Node
 from sec_interfaces.msg import StartSwitch
 
-start_switch = DigitalInputDevice(10)
-
 # from rpi_hardware_pwm import HardwarePWM
 
-class StartSwitch(Node):
+class StartSwitchDriver(Node):
    def __init__(self):
       super().__init__("start_switch")
       self.start_switch = DigitalInputDevice(10)
@@ -18,18 +16,18 @@ class StartSwitch(Node):
 
    def timer_callback(self):
       msg = StartSwitch()
-      if (start_switch.is_active):
+      if (self.start_switch.is_active):
          self.get_logger().info(f"Start switch pressed")
          msg.start_switch_pressed = True
       else:
          msg.start_switch_pressed = False
-      self.publisher.publish(msg)
+      self.start_switch_publisher.publish(msg)
 
 
 def main(args=None):
    rclpy.init(args=args)
    
-   start_switch_node = StartSwitch()
+   start_switch_node = StartSwitchDriver()
    
    rclpy.spin(start_switch_node)
 
