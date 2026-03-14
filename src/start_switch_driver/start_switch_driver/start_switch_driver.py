7b5d1abfc3f52b89cd18ec26b0c4927febe6b353
Will Gibbs <will.gibbs7793@gmail.com>
2026-03-14 01:43:21 -0500

    Change start switch from digital input device to button

--- src/start_switch_driver/start_switch_driver/start_switch_driver.py
+++ src/start_switch_driver/start_switch_driver/start_switch_driver.py
@@ -1,4 +1,4 @@
-from gpiozero import DigitalInputDevice
+from gpiozero import Button
 from time import sleep
 
 import rclpy
@@ -8,13 +8,13 @@ from sec_interfaces.msg import StartSwitch
 class StartSwitchDriver(Node):
    def __init__(self):
       super().__init__("start_switch")
-      self.start_switch = DigitalInputDevice(10)
+      self.start_switch = Button(16, pull_up=False)
       self.start_switch_publisher = self.create_publisher(StartSwitch, "start_switch", 10)
       self.timer = self.create_timer(0.1, self.timer_callback)
 
    def timer_callback(self):
       msg = StartSwitch()
-      if (self.start_switch.is_active):
+      if (self.start_switch.is_pressed):
          self.get_logger().info(f"Start switch pressed")
          msg.start_switch_pressed = True
       else:
